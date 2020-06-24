1e9721d035c9f38f432376bbbd8d1eb58b0143bf
DecoJesus <valdeque.de.ramos@hotmail.com>
2020-06-24 18:38:53 -0300

    Adionando a Classe MatriculaAluno pra pode adicionar os dados da tabela Turma no formulário de cadastro de Aluno

--- ControlerAcessoAluno/ControlerAcessoAluno/Controllers/AlunoController.cs
+++ ControlerAcessoAluno/ControlerAcessoAluno/Controllers/AlunoController.cs
@@ -38,6 +38,20 @@ namespace ControlerAcessoAluno.Controllers
         // GET: Aluno/Create
         public ActionResult Create()
         {
+            //Aqui está montando o conteúdo a ser apresentado
+            //no combobox de turma. Value é o valor que será submetido
+            //caso o item seja selecionado e Text é o valor que será 
+            //apresentado no campo.
+            //Seria muito se eu tivesse adicionado "Turma"(A, B, C, D e etc) por exemplo.
+            //Acho que estám faltando isso no seu modelo  
+
+            var Turmas = db.TB_TURMA.Select(t => new SelectListItem
+            {
+                Value = t.COD_TURMA + "",
+                Text = t.TB_CURSO.NOME_CURSO.Substring(0, 3) + t.SERIE + t.PERIODO.Substring(0, 1)
+            });
+            ViewBag.Turmas = Turmas;
+
             return View();
         }
 
@@ -46,11 +60,29 @@ namespace ControlerAcessoAluno.Controllers
         // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "COD_ALUNO,NOME,RM,DATA_NASCIMENTO,SEXO")] TB_ALUNO tB_ALUNO)
+        //public ActionResult Create([Bind(Include = "COD_ALUNO,NOME,RM,DATA_NASCIENTO,SEXO")] TB_ALUNO tB_ALUNO)
+        public ActionResult Create(MatriculaAluno matricula)//vai pegar os dados que estão inseridos na classe MatriculaAluno
         {
+
+            TB_ALUNO tB_ALUNO = matricula.TB_ALUNO;
+
             if (ModelState.IsValid)
             {
                 db.TB_ALUNO.Add(tB_ALUNO);
+                db.SaveChanges();
+
+                TB_TURMA tB_TURMA = db.TB_TURMA.Find(matricula.COD_TURMA);
+                TB_ALUNO_TURMA tB_ALUNO_TURMA = new TB_ALUNO_TURMA
+                {
+                    ANO = DateTime.Today.Year + "",
+                    COD_ALUNO = tB_ALUNO.COD_ALUNO,
+                    COD_TURMA = tB_TURMA.COD_TURMA,
+                    SEMESTRE = "1", //obs: Precisa ver de onde vai tirar essa informacao. 
+                    //Acredito que o semestre deveria estar acessível através da turma, 
+                    //mas turma não tem essa informação, entao, acho que está faltando...
+
+                };
+                db.TB_ALUNO_TURMA.Add(tB_ALUNO_TURMA);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
@@ -114,6 +146,9 @@ namespace ControlerAcessoAluno.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+
+
 
         protected override void Dispose(bool disposing)
         {
